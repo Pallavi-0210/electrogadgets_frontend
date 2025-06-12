5eafc106d01b7b1efd336533a1d4c1b4b968138e
Pallavi-0210 <pallavitejaswi596@gmail.com>
2025-06-12 19:56:13 +0530

    Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -11,7 +11,7 @@ function Login() {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:5000/api/login', {
+            const response = await fetch('https://electrogadgets-backend.onrender.com/api/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 credentials: 'include',
